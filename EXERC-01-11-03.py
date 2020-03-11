0dd3ca639e2b018b29663cd373954eae7f0207cc
MarcosBan <marcos.mps10@gmail.com>
2020-03-11 11:31:26 -0300

    Add files via upload

---
+++ EXERC-01-11-03.py
@@ -0,0 +1,13 @@
+"""
+Programa que soma dois numeros
+Autor: Marcos Silva
+Data: 11/03/2020
+"""
+#Comentario de uma unica linha
+num1 = float(input("Informe o primeiro numero: "))
+num2 = float(input("Informe o segundo numero: "))
+
+soma = num1 + num2 #Soma dois numeros
+
+print("A soma de ", num1, "e ", num2," é igual: ", soma)
+input("Pressione Enter para sair do programa")
